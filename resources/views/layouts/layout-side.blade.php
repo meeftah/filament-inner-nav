138c370b5a87f0dd460762516318e11fcc843500
meeftah <miftah.yuda@gmail.com>
2024-04-20 02:53:54 +0700

    apply sticky navigation

--- resources/views/layouts/layout-side.blade.php
+++ resources/views/layouts/layout-side.blade.php
@@ -67,6 +67,7 @@
         <ul @class([
             'space-y-2 font-inter font-medium',
             'mt-4' => filled($navTitle) || filled($navDescription),
+            'fi-inner-nav-sticky' => $innerNav->isSticky(),
         ])>
             @foreach ($innerNav->getNavigationItems() as $item)
                 @if ($item instanceof \Rawilk\FilamentInnerNav\InnerNavGroup)
